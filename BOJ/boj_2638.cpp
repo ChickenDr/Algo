9f90f70097a09f5b246b40bba507c87073b3ad04
똥수 <fkdlrj1230@gmail.com>
2021-12-15 16:58:31 +0900

    12.15

--- BOJ/boj_2638.cpp
+++ BOJ/boj_2638.cpp
@@ -5,23 +5,23 @@
 #include <queue>
 
 using namespace std;
-int n, m, map[101][101], t_map = [101][101];
+int n, m, level = 0, map[101][101], t_map[101][101];
 int x_e[4] = {1, -1, 0, 0}, y_e[4] = {0, 0, 1, -1};
-bool visit[101][101];
+bool visit[101][101], a_visit[101][101], a_map[101][101];
 
 void dfs(int x, int y){
     visit[x][y] = true;
 
     for (int i = 0; i < 4; i++)
     {
-        int m_x = x + x_e;
-        int m_y = y + y_e;
+        int m_x = x + x_e[i];
+        int m_y = y + y_e[i];
         if(m_x < 0 || m_x > n || m_y > m || m_y < 0 || visit[m_x][m_y]) continue;
-        else dfs(mx, my);
+        else dfs(m_x, m_y);
     }
 }
 
-void malt(){
+void melt(){
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
@@ -35,20 +35,55 @@ void malt(){
         int cnt = 0;
         for (int j = 0; j < m; j++)
         {
-            
+            if(map[i][j]){
+                for (int k = 0; k < 4; k++)
+                {
+                    int m_x = i + x_e[k];
+                    int m_y = j + y_e[k];
+
+                    if(m_x < 0 || m_x > n || m_y > m || m_y < 0 || !a_map[m_x][m_y]) continue;
+                    cnt++;
+                }
+
+                if(cnt >= 2){
+                    map[i][j]--;
+                }
+            }
+            cnt = 0;
         }
     }
     
 }
 
-bool in_air(int x, int y){
-    queue<pair<int, int> q;
+void out_air(){
+    queue<pair<int, int>> Q;
+    Q.push(make_pair(0, 0));
+    a_visit[0][0] = true;
+
+    while(!Q.empty()){
+        pair<int, int> curr = Q.front();
+        Q.pop();
+
+        int x = curr.first, y = curr.second;
+
+        for (int k = 0; k < 4; k++)
+        {
+            int m_x = x + x_e[k];
+            int m_y = y + y_e[k];
+
+            if(m_x < 0 || m_x > n || m_y > m || m_y < 0 || a_visit[m_x][m_y] || map[m_x][m_y] > 0) continue;
+            else{
+                a_map[m_x][m_y] = true;
+                a_visit[m_x][m_y] = true;
+                Q.push(make_pair(m_x, m_y));
+            }
+        }
+    }
 }
 
 int main(){
     cin >> n >> m;
 
-    int cnt = 0;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
@@ -59,6 +94,11 @@ int main(){
     
     while(1){
         memset(visit, false, sizeof(visit));
+        memset(a_map, false, sizeof(a_map));
+        memset(a_visit, false, sizeof(a_visit));
+        out_air();
+
+        int cnt = 0;
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < m; j++)
@@ -70,7 +110,13 @@ int main(){
             }
         }
 
+        if (cnt == 0) break;
+        
+        level++;
+        melt();
         cnt = 0;
     }
+
+    cout << level;
     return 0;
 }
