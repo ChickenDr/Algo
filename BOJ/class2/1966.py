b907639fed7b30090d1be1d2e491fb226fe29814
dev-dong-su <fkdlrj1230@gmail.com>
2023-03-07 08:53:16 +0900

    0307

---
+++ BOJ/class2/1966.py
@@ -0,0 +1,17 @@
+import sys
+
+global arr, ran, target
+
+
+def solve():
+    for i in arr:
+        if ()
+    return
+
+
+if __name__ == "__main__":
+    loop = int(input())
+    while (loop):
+        ran, target = input().split()
+        arr = list(map(int, sys.stdin.readline().split()))
+        loop -= 1
