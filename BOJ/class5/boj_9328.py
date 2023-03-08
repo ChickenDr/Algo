9aca6da7158873610e262bd1d630c3ee99f27a39
Hee Su <fkdlrj1230@gmail.com>
2023-03-08 17:46:10 +0900

    asd

--- BOJ/class5/boj_9328.py
+++ BOJ/class5/boj_9328.py
@@ -2,21 +2,29 @@ import sys
 
 room = []
 visit = []
+key = []
 mx = [1, -1, 0, 0]
 my = [0, 0, -1, 1]
 
-def solve():
+def solve(x, y):
+    q = []
+    q.append([x, y])
+
+
+        
     return
 
 if __name__ == "__main__":
     loop = int(input())
-
     for _ in range(loop):
         h, w  = map(int, input().split())
         visit = [[False * w] * h]
 
         for _ in range(h):
-            room.append(list(map(int, sys.stdin.readline().split())))
+            room.append(list(input()))
         
+        key = list(input())
+
+        print(room)
 
         
