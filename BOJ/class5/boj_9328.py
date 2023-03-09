424767900dea04f9c18f09a3799bee67f30e8e29
Hee Su <fkdlrj1230@gmail.com>
2023-03-09 17:56:24 +0900

    0309

--- BOJ/class5/boj_9328.py
+++ BOJ/class5/boj_9328.py
@@ -9,22 +9,27 @@ my = [0, 0, -1, 1]
 def solve(x, y):
     q = []
     q.append([x, y])
-
-
-        
     return
 
 if __name__ == "__main__":
     loop = int(input())
     for _ in range(loop):
         h, w  = map(int, input().split())
-        visit = [[False * w] * h]
+        
+        visit = [[0] * (w + 1)] * (h + 1)
+        print(visit)
+        visit.insert(0, [0 * (w)])
+        
+
+        for i in range(h):
+            visit[i].insert(0, 0)
+            visit[i].append(0, 0)
 
         for _ in range(h):
             room.append(list(input()))
         
-        key = list(input())
+        key = list(input().split())
 
         print(room)
+        print(key)
 
-        
