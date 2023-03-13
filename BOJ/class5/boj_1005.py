c9f0fb750a64130457b7aac80967b3973aa51398
Hee Su <fkdlrj1230@gmail.com>
2023-03-13 17:40:07 +0900

    0313

---
+++ BOJ/class5/boj_1005.py
@@ -0,0 +1,40 @@
+import heapq
+
+tmp = []
+resualt = []
+target  = 0
+
+def solve():
+    while tmp:
+        x = heapq.heappop(tmp)
+        resualt.append(x)
+
+        for nx in graph[x]:
+            degree[nx] -= 1
+
+            if degree[nx] == 0:
+                heapq.heappush(tmp, nx)
+    return resualt
+
+
+if __name__ == "__main__":
+    loop = int(input())
+    for _ in range(loop):
+        n, k = map(int, input().split())
+        arr = list(map(int, input().split()))
+
+        graph = [[] for _ in range(n + 1)]
+        degree = [0] * (n + 1)
+            
+        for _ in range(k):
+            a, b = map(int, input().split())
+            graph[a].append(b)
+            degree[b] += 1
+        
+        target = input()
+            
+        for i in range(1, n + 1):
+            if degree[i] == 0:
+                heapq.heappush(tmp, i)
+        
+        print(f'{solve()}')
--- BOJ/class5/boj_9328.py
+++ BOJ/class5/boj_9328.py
@@ -2,34 +2,61 @@ import sys
 
 room = []
 visit = []
-key = []
-mx = [1, -1, 0, 0]
-my = [0, 0, -1, 1]
 
-def solve(x, y):
+
+def solve(w, h):
+    global visit, room
+
+    door = []
+    key = []
+    resualt = 0
     q = []
-    q.append([x, y])
-    return
+    visit[0][0] = 1
+    mx = [1, -1, 0, 0]
+    my = [0, 0, -1, 1]
+    
+    q.append([0, 0])
+   
+    while q:
+        curr = q.pop(0)
+        for i in range(0, 4):
+            cx = curr[1] + mx[i]
+            cy = curr[0] + my[i]
+
+            if cy < 0 or cx < 0 or cx > w + 1 or  cy > h + 1 or visit[cy][cx] == 1 or room[cy][cx] == '*':
+                continue
+
+    
+    key.clear()
+    door.clear()
+    visit.clear()
+    room.clear()
+    
+    return resualt
 
 if __name__ == "__main__":
+    
     loop = int(input())
+
     for _ in range(loop):
         h, w  = map(int, input().split())
         
-        visit = [[0] * (w + 1)] * (h + 1)
-        print(visit)
-        visit.insert(0, [0 * (w)])
-        
-
-        for i in range(h):
-            visit[i].insert(0, 0)
-            visit[i].append(0, 0)
+        visit = [[0] * (w + 2)]
+        for _ in range(h + 1):
+            visit.append([0] * (w + 2))
 
         for _ in range(h):
             room.append(list(input()))
         
-        key = list(input().split())
+        for i in range(h):
+            room[i].insert(0, '.')
+            room[i].append('.')
+        room.insert(0, ['.'] * (w + 2))
+        room.append(['.'] * (w + 2))
 
-        print(room)
-        print(key)
 
+        tmp = input()
+        if tmp != '0':
+            key = list(tmp)
+
+        print(f'{solve(w, h)}')
